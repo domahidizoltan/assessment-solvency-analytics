6ad202564c75fb5c79c8d34f45c91167fc5631e2
Zoltán Domahidi <domahidizoltan@gmail.com>
2024-01-31 10:11:20 +0100

    implement findFirstOccurance

--- algorithmictask/algorithmictask.go
+++ algorithmictask/algorithmictask.go
@@ -1,5 +1,30 @@
 package solvencyanalytics
 
+import (
+	"strconv"
+	"strings"
+)
+
 func findFirstOccurance(haystack, needle []int) []int {
-	return nil
+	var needleIdx int
+	results := make([]int, 0, len(needle))
+	for i, h := range haystack {
+		if contains(h, needle[needleIdx]) {
+			results = append(results, i)
+			needleIdx++
+		}
+
+		if needleIdx >= len(needle) {
+			break
+		}
+	}
+
+	return results
+}
+
+func contains(number int, digit int) bool {
+	numberStr := strconv.Itoa(number)
+	digitStr := strconv.Itoa(digit)
+
+	return strings.Contains(numberStr, digitStr)
 }
